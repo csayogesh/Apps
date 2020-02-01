f64da341c082a41710355703aa6c095ce5f68dd8
Yogi <bhosay@amazon.com>
2020-02-01 15:41:46 +0530

    1. Configured surefire to autorun unit tests on build 2. Removed faulty dependency on the OpenCsvWrapper

--- OpenCsvWrapper/src/test/java/com/lib/csv/CsvFileReaderTest.java
+++ OpenCsvWrapper/src/test/java/com/lib/csv/CsvFileReaderTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class CsvFileReaderTests {
+public class CsvFileReaderTest {
 
     @Test
     public void simpleReaderHeaderTests() throws IOException {
